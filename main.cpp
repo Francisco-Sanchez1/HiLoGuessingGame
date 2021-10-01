9d29e43abae64000ea6b4d4e5d1777a048179d96
sanfrank21 <franksanchez132@gmail.com>
2021-09-30 23:45:23 -0500

    third commit

---
+++ main.cpp
@@ -0,0 +1,50 @@
+
+#include <iostream>
+#include <string>
+using namespace std; 
+
+
+int main() 
+{
+  int n;
+  
+  cout << "Let’s play a Number Guessing Game!  What is the highest possible value of a number you might pick? (Choose n) "; 
+  cin >> n;
+  cin.ignore();
+  cout <<"\nCool, you picked " << n <<". Now please think of a number from 1 to " << n << " and write it down, then press ENTER and we’ll play.\n\n";
+  cin.get();
+  
+  int l = 1;
+  int r = n;
+  string ans;
+  int counter = 1; 
+  bool done = false;
+
+  
+  while(!done)
+  {
+    int m = l + (r-l) / 2;
+    cout << "Is you number " << m << "? ";
+    getline(cin, ans);  
+
+    if(ans == "Too low" || ans == "too low")
+    {
+     l = m + 1;
+     counter++;
+     
+     
+    }
+    if(ans == "Too high"|| ans == "too high")
+    {
+      r = m - 1;
+      counter++;
+    }
+    if(ans == "Yes" || ans =="yes")
+    {
+      cout << "Awesome, I got the answer in " <<counter<<" guesses.  I’m a super smart program.";
+      done = true;
+    }
+  }
+
+
+}
